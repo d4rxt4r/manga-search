2468db0295193408b5c9f8225f233e6fd4a20c6b
D4RKST4R <rakow47@gmail.com>
2020-06-23 16:44:32 +0300

    cookies!

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function getStatus(id, site) {
 }
 
 function getTitlesFromPage(siteId, offset = 0) {
-    $.getJSON('https://api.allorigins.win/get?url=' + encodeURIComponent(`${baseUrls[siteId-1]}/list?sortType=rate&filter=translated&offset=${offset}`),
+    $.getJSON('https://api.allorigins.win/get?url=' + encodeURIComponent(`${baseUrls[siteId - 1]}/list?sortType=rate&filter=translated&offset=${offset}`),
         data => {
             const tiles = $(data.contents).find('.tile');
             const tilesTag = tiles.find('.tags');
@@ -34,31 +34,34 @@ function getTitlesFromPage(siteId, offset = 0) {
             resultsDiv.classList.add('results');
             document.body.append(resultsDiv);
 
-            console.log(`total - ${titles.length}`);
+            if (titles.length == 0) {
+                const emptyResults = document.getElementById('noResults').content.cloneNode(true);
+				resultsDiv.append(emptyResults);
+            } else {
+                let link, linkWrap, refreshStatus;
+                for (let i = 0; i < titles.length; i++) {
+                    linkWrap = document.createElement('div');
+                    linkWrap.style = 'display: block; font-size: 1.5em; padding: .5em';
 
-            let link, linkWrap, refreshStatus;
-            for (let i = 0; i < titles.length; i++) {
-                linkWrap = document.createElement('div');
-                linkWrap.style = 'display: block; font-size: 1.5em; padding: .5em';
+                    refreshStatus = document.createElement('span');
+                    refreshStatus.innerHTML = '<i class="icon icon-refresh"></i>';
+                    refreshStatus.style = 'margin-left: 20px;';
+                    refreshStatus.setAttribute('onclick', `getStatus(${bookmarks[i].dataset['id']}, ${siteId})`);
 
-                refreshStatus = document.createElement('span');
-                refreshStatus.innerHTML = '<i class="icon icon-refresh"></i>';
-                refreshStatus.style = 'margin-left: 20px;';
-                refreshStatus.setAttribute('onclick', `getStatus(${bookmarks[i].dataset['id']}, ${siteId})`);
+                    link = document.createElement('a');
+                    link.innerText = titles[i].title;
+                    if (tilesTag[i].children.length != 2) {
+                        link.innerText += ' (Сингл)';
+                    }
+                    link.target = '_blank';
+                    link.href = baseUrls[siteId - 1] + titles[i].pathname;
+                    link.dataset['id'] = bookmarks[i].dataset['id'];
 
-                link = document.createElement('a');
-                link.innerText = titles[i].title;
-                if (tilesTag[i].children.length != 2) {
-                    link.innerText += ' (Сингл)';
-                }
-                link.target = '_blank';
-                link.href = baseUrls[siteId-1] + titles[i].pathname;
-                link.dataset['id'] = bookmarks[i].dataset['id'];
-                
-                linkWrap.append(link, refreshStatus);
+                    linkWrap.append(link, refreshStatus);
 
-                resultsDiv.append(linkWrap);
-            };
+                    resultsDiv.append(linkWrap);
+                };
+            }
         }
     );
 }
@@ -66,8 +69,20 @@ function getTitlesFromPage(siteId, offset = 0) {
 const loadBtn = document.getElementById('load');
 let siteSelect = document.getElementById('siteSelect');
 let pageInput = document.getElementById('pageInput');
-loadBtn.addEventListener('click', () => {
-    getTitlesFromPage(siteSelect.value, (pageInput.value - 1) * 70)
-});
 
-getTitlesFromPage(1, 0);
+let siteIdCookie = Cookies.get('siteId');
+let pageNumCookie = Cookies.get('pageNum');
+
+if (siteIdCookie != undefined && pageNumCookie != undefined) {
+	siteSelect.value = siteIdCookie;
+    pageInput.value = pageNumCookie;
+    getTitlesFromPage(siteSelect.value, (pageInput.value - 1) * 70);
+} else {
+    getTitlesFromPage(1, 0);
+}
+
+loadBtn.addEventListener('click', () => {
+    Cookies.set('siteId', siteSelect.value, { expires: 365 });
+    Cookies.set('pageNum', pageInput.value, { expires: 365 });
+    getTitlesFromPage(siteSelect.value, (pageInput.value - 1) * 70);
+});
