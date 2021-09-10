9e3de274a43aa2b716227966c4e6de62a6ec5ffa
d4rxt4r <rakow47@gmail.com>
2021-09-10 18:20:42 +0300

    removed shakai (not working)
    fixed mintmanga
    added favicon

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const proxyUrl = 'https://api.allorigins.win/raw?url=';
-const baseUrls = ['https://readmanga.live', 'https://mintmanga.live', 'http://shakai.ru/catalog/manga/']
-const searchUrls = [`${baseUrls[0]}/list?sortType=votes&filter=translated`, `${baseUrls[1]}/list/tag/noyaoi?sortType=rate&filter=translated`];
+const baseUrls = ['https://readmanga.live', 'https://mintmanga.live', 'https://shakai.ru/catalog/manga/']
+const searchUrls = [`${baseUrls[0]}/list?sortType=votes&filter=translated`, `${baseUrls[1]}/list?sortType=votes&filter=translated`];
+const userID = 582791;
 
 function setStatus(data) {
     const script = document.querySelector('.status-script');
@@ -18,53 +19,51 @@ function getStatus(id, siteId) {
     const statusScript = document.createElement('script');
     statusScript.attributes.id = id;
     statusScript.classList.add('status-script');
-    statusScript.src = `https://grouple.co/external/status?callback=setStatus&id=${id}&site=${siteId}&user=582791`
+    statusScript.src = `https://grouple.co/external/status?callback=setStatus&id=${id}&site=${siteId}&user=${userID}`
     document.body.append(statusScript);
 }
 
-async function getTitlesFromPage(siteId, offset = 0) {
-    if (document.querySelector('.results')) document.querySelector('.results').remove();
+function createLink(title, bookmark, siteId, grouple=true, single=false, yaoi=false) {
+    const linkWrap = document.createElement('div');
+    const link = document.createElement('a');
+    
+    linkWrap.classList.add('link-wrap');
+    
+    link.innerText = title.title;    
+    if (single) {
+        link.innerText += ' (Сингл)';
+    }
+    
+    link.target = '_blank';
+    link.href = baseUrls[siteId - 1] + title.pathname;
+    link.dataset['id'] = bookmark.dataset['id'];
+    
+    linkWrap.append(link);
+    
+    if (yaoi) {
+        const chip = document.createElement('div');
+        chip.classList.add('chip', 'bg-error');
+        chip.innerText = 'yaoi'
+        linkWrap.append(chip);
+    }
+    
+    if (grouple) {
+        const refreshStatus = document.createElement('span');
+        refreshStatus.innerHTML = '<i class="icon icon-refresh"></i>';
+        refreshStatus.setAttribute('onclick', `getStatus(${bookmark.dataset.id}, ${siteId})`);
+        linkWrap.append(refreshStatus);
+    }
 
-    const resultsDiv = document.createElement('div');
-    resultsDiv.classList.add('results');
-    document.body.append(resultsDiv);
+    return linkWrap;
+}
 
-    if (siteId == 3) {
-        const url = proxyUrl + encodeURIComponent(baseUrls[2] + offset);
-        await fetch(url)
+async function getGroupleManga(siteId, url, resultsDiv) {
+    await fetch(url)
             .then(response => { return response.text() })
             .then(data => {
-                const parser = new DOMParser();
-                const parsed_document = parser.parseFromString(data, 'text/html');
 
-                const posters = parsed_document.querySelectorAll('.poster');
+                console.log(data);
 
-                if (posters.length == 0) {
-                    const emptyResults = document.getElementById('noResults').content.cloneNode(true);
-                    resultsDiv.append(emptyResults);
-                } else {
-                    for (let i = 0; i < posters.length; i++) {
-                        const description = posters[i].querySelectorAll('.poster__float-description');
-                        const release = description[1].innerText;
-                        const genres = description[2].innerText
-                        if (!release.includes('Завершен') && genres.includes('Яой')) continue;
-                        const linkWrap = document.createElement('div');
-                        const link = document.createElement('a');
-                        link.innerText = posters[i].querySelector('.poster__float-heading').innerText.match(/[^/]+$/gm)[0];
-                        link.href = posters[i].href;
-                        link.target = '_blank';
-                        linkWrap.append(link);
-
-                        linkWrap.style = 'display: block; font-size: 1.5em; padding: .5em';
-                        resultsDiv.append(linkWrap);
-                    }
-                }
-            });
-    } else {
-        url = proxyUrl + encodeURIComponent(`${searchUrls[siteId - 1]}&offset=${(offset - 1) * 70}`);
-        await fetch(url)
-            .then(response => { return response.text() })
-            .then(data => {
                 const parser = new DOMParser();
                 const parsed_document = parser.parseFromString(data, 'text/html');
 
@@ -75,32 +74,80 @@ async function getTitlesFromPage(siteId, offset = 0) {
                     resultsDiv.append(emptyResults);
                 } else {
                     for (let i = 0; i < tiles.length; i++) {
-                        const linkWrap = document.createElement('div');
-                        linkWrap.style = 'display: block; font-size: 1.5em; padding: .5em';
-
-                        const refreshStatus = document.createElement('span');
                         const bookmark = tiles[i].querySelector('span.bookmark-menu');
-                        refreshStatus.innerHTML = '<i class="icon icon-refresh"></i>';
-                        refreshStatus.style = 'margin-left: 20px;';
-                        refreshStatus.setAttribute('onclick', `getStatus(${bookmark.dataset.id}, ${siteId})`);
-
-                        const link = document.createElement('a');
                         const title = tiles[i].querySelector('h3 a');
-                        const tileTag = tiles[i].querySelector('.tags');
-                        link.innerText = title.title;
-                        if (tileTag.children.length != 2) {
-                            link.innerText += ' (Сингл)';
+                        const tags = tiles[i].querySelectorAll('.element-link');
+
+                        let yaoi = false;
+                        tags.forEach(t => {
+                            if (t.innerText == 'яой') {
+                                yaoi = true;
+                            }
+                        });
+
+                        let single = false;
+                        if (tiles[i].querySelector('.mangaSingle')) {
+                            single = true;
                         }
-                        link.target = '_blank';
-                        link.href = baseUrls[siteId - 1] + title.pathname;
-                        link.dataset['id'] = bookmark.dataset['id'];
 
-                        linkWrap.append(link, refreshStatus);
-
-                        resultsDiv.append(linkWrap);
+                        const link = createLink(title, bookmark, siteId, grouple=true, single=single, yaoi=yaoi)
+                        resultsDiv.append(link);
                     };
                 }
             });
+}
+
+async function getShakaiManga(url, resultsDiv) {
+    await fetch(url)
+        .then(response => { return response.text() })
+        .then(data => {
+            console.log(data);
+        });
+}
+
+function getTitlesFromPage(siteId, offset = 0) {
+    if (document.querySelector('.results')) document.querySelector('.results').remove();
+
+    const resultsDiv = document.createElement('div');
+    resultsDiv.classList.add('results');
+    document.body.append(resultsDiv);
+
+    // shakai
+    if (siteId == 3) {
+        const url = proxyUrl + encodeURIComponent(baseUrls[2] + offset);
+        getShakaiManga(url, resultsDiv);
+        // await fetch(url)
+        //     .then(response => { return response.text() })
+        //     .then(data => {
+        //         const parser = new DOMParser();
+        //         const parsed_document = parser.parseFromString(data, 'text/html');
+
+        //         const posters = parsed_document.querySelectorAll('.poster');
+
+        //         if (posters.length == 0) {
+        //             const emptyResults = document.getElementById('noResults').content.cloneNode(true);
+        //             resultsDiv.append(emptyResults);
+        //         } else {
+        //             for (let i = 0; i < posters.length; i++) {
+        //                 const description = posters[i].querySelectorAll('.poster__float-description');
+        //                 const release = description[1].innerText;
+        //                 const genres = description[2].innerText
+        //                 if (!release.includes('Завершен') && genres.includes('Яой')) continue;
+        //                 const linkWrap = document.createElement('div');
+        //                 const link = document.createElement('a');
+        //                 link.innerText = posters[i].querySelector('.poster__float-heading').innerText.match(/[^/]+$/gm)[0];
+        //                 link.href = posters[i].href;
+        //                 link.target = '_blank';
+        //                 linkWrap.append(link);
+
+        //                 linkWrap.style = 'display: block; font-size: 1.5em; padding: .5em';
+        //                 resultsDiv.append(linkWrap);
+        //             }
+        //         }
+        //     });
+    } else {
+        const url = proxyUrl + encodeURIComponent(`${searchUrls[siteId - 1]}&offset=${(offset - 1) * 70}`);
+        getGroupleManga(siteId, url, resultsDiv);
     }
 }
 
